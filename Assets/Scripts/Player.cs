86c087dfeab121d37927dbd4adc79712988f5fe4
Marcus Williamson <hamawill@ucsc.edu>
2024-06-13 19:35:25 -0700

    PD/OSC implementation, escape to exit and pause music

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityOSC;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+
+    private float speed = 0f;
+    private bool isMoving = false;  // For playing footstep noise
 
     public SpriteRenderer playerSpriteRenderer;
     public Animator animator;
@@ -23,7 +27,8 @@ public class Player : MonoBehaviour
     Vector2 movement;
     void Start()
     {
-        
+        OSCHandler.Instance.Init();
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/playMusic", 1);
     }
 
     // Update is called once per frame
@@ -31,12 +36,41 @@ public class Player : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        speed = movement.sqrMagnitude;
+        animator.SetFloat("Speed", speed);
+
+        if (!isMoving && speed > 0)
+        {
+            isMoving = true;
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/walking", 1);
+        } else if (isMoving && speed < 0.1)
+        {
+            isMoving = false;
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/walking", 0);
+        }
+
         if (Input.GetKeyDown(KeyCode.X)){
             ShootBullet();
-            
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/gunshot", 1);
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/playMusic", 0);
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/walking", 0);
+            //If we are running in a standalone build of the game
+            #if UNITY_STANDALONE
+            //Quit the application
+            Application.Quit();
+            #endif
+
+            //If we are running in the editor
+            #if UNITY_EDITOR
+            //Stop playing the scene
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+
     }
 
     private void FixedUpdate() 
